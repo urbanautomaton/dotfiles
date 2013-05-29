b72372fac1638c895126ae428dd4b61235420fb0
Simon Coffey <simon@tribesports.com>
2013-05-29 12:03:17 +0100

    Fix install.sh after rename

--- install.sh
+++ install.sh
@@ -1,27 +1,27 @@
 #! /bin/sh
 
-ln -sf ~/conf/bashrc ~/.bashrc
-ln -sf ~/conf/bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/bash_aliases ~/.bash_aliases
 
-ln -sf ~/conf/vimrc ~/.vimrc
-ln -sf ~/conf/vim ~/.vim
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim ~/.vim
 
-ln -sf ~/conf/irbrc ~/.irbrc
-ln -sf ~/conf/gemrc ~/.gemrc
+ln -sf ~/dotfiles/irbrc ~/.irbrc
+ln -sf ~/dotfiles/gemrc ~/.gemrc
 
-ln -sf ~/conf/git-completion ~/.git-completion
-ln -sf ~/conf/gitconfig ~/.gitconfig
-ln -sf ~/conf/git_template ~/.git_template
-ln -sf ~/conf/gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/git-completion ~/.git-completion
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/git_template ~/.git_template
+ln -sf ~/dotfiles/gitignore_global ~/.gitignore_global
 
-ln -sf ~/conf/js ~/.js
-ln -sf ~/conf/gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/js ~/.js
+ln -sf ~/dotfiles/gvimrc ~/.gvimrc
 
-ln -sf ~/conf/ackrc ~/.ackrc
+ln -sf ~/dotfiles/ackrc ~/.ackrc
 
-ln -sf ~/conf/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
 
 mkdir -p ~/bin
-for script in $(ls ~/conf/bin); do
-  ln -sf ~/conf/bin/$script ~/bin/$script
+for script in $(ls ~/dotfiles/bin); do
+  ln -sf ~/dotfiles/bin/$script ~/bin/$script
 done
