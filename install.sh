cd85bfeb0340b3c574bfee84de4b52f9651ffeb0
Simon Coffey <simon@tribesports.com>
2013-08-09 19:32:05 +0100

    Don't recursively link dirs

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@ ln -sf ~/dotfiles/bashrc ~/.bashrc
 ln -sf ~/dotfiles/bash_aliases ~/.bash_aliases
 
 ln -sf ~/dotfiles/vimrc ~/.vimrc
-ln -sf ~/dotfiles/vim ~/.vim
+ln -sfh ~/dotfiles/vim ~/.vim
 
 ln -sf ~/dotfiles/irbrc ~/.irbrc
 ln -sf ~/dotfiles/gemrc ~/.gemrc
 
 ln -sf ~/dotfiles/git-completion ~/.git-completion
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/git_template ~/.git_template
+ln -sfh ~/dotfiles/git_template ~/.git_template
 ln -sf ~/dotfiles/gitignore_global ~/.gitignore_global
 
-ln -sf ~/dotfiles/js ~/.js
+ln -sfh ~/dotfiles/js ~/.js
 ln -sf ~/dotfiles/gvimrc ~/.gvimrc
 
 ln -sf ~/dotfiles/ackrc ~/.ackrc
