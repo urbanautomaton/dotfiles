f658aeedbb312b5bf77e71ff2300eac07958451b
Simon Coffey <simon@tribesports.com>
2014-08-26 17:56:51 +0100

    Use debug trap hack for ruby_auto_env
    This means our hook will work for non-interactive sessions. It means in
    interactive sessions the hook will only fire when the first interactive
    command is run (i.e. not as soon as you cd into the directory).

--- bin/ruby_env_auto.sh
+++ bin/ruby_env_auto.sh
@@ -1,3 +1,11 @@
+function chruby_default() {
+  if [[ -n "$CHRUBY_DEFAULT" ]]; then
+    chruby "$CHRUBY_DEFAULT"
+  else
+    chruby_reset
+  fi
+}
+
 function ruby_env_auto() {
   local current_dir="$PWD"
   local version
@@ -6,7 +14,6 @@ function ruby_env_auto() {
 		if { read -r version <"$current_dir/.ruby-version"; } 2>/dev/null; then
 			if [[ "$version" == "$RUBY_AUTO_VERSION" ]]; then return
 			else
-        echo "Setting up Ruby $version workspace in ${current_dir}..."
 				RUBY_AUTO_VERSION="$version"
 				chruby "$version" && gem_home "$current_dir"
 				return $?
@@ -17,13 +24,8 @@ function ruby_env_auto() {
   done
 
   if [[ -n "$RUBY_AUTO_VERSION" ]]; then
-    echo "Resetting Ruby environment..."
     unset RUBY_AUTO_VERSION
-    if [[ -n "$CHRUBY_DEFAULT" ]]; then
-      chruby "$CHRUBY_DEFAULT"
-    else
-      chruby_reset
-    fi
+    chruby_default
   fi
 }
 
@@ -31,12 +33,6 @@ if [[ -n "$ZSH_VERSION" ]]; then
 	if [[ ! "$preexec_functions" == *ruby_env_auto* ]]; then
 		preexec_functions+=("ruby_env_auto")
 	fi
-else
-	if [[ -n "$PROMPT_COMMAND" ]]; then
-		if [[ ! "$PROMPT_COMMAND" == *ruby_env_auto* ]]; then
-			PROMPT_COMMAND="$PROMPT_COMMAND; ruby_env_auto"
-		fi
-	else
-		PROMPT_COMMAND="ruby_env_auto"
-	fi
+elif [[ -n "$BASH_VERSION" ]]; then
+  trap '[[ "$BASH_COMMAND" != "$PROMPT_COMMAND" ]] && ruby_env_auto' DEBUG
 fi
