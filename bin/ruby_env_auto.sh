e10ca5920d9b8d6fdd05b8eddff47b11baae781d
Simon Coffey <simon@urbanautomaton.com>
2015-07-03 23:38:15 +0100

    Remove ruby GC tuning from generic workspace

--- bin/ruby_env_auto.sh
+++ bin/ruby_env_auto.sh
@@ -6,41 +6,6 @@ function chruby_default() {
   fi
 }
 
-function ruby_env_reset_gc() {
-  # Ruby 2.1 variables
-  unset RUBY_GC_HEAP_INIT_SLOTS
-  unset RUBY_GC_HEAP_FREE_SLOTS
-  unset RUBY_GC_HEAP_GROWTH_FACTOR
-  unset RUBY_GC_HEAP_GROWTH_MAX_SLOTS
-
-  # Ruby < 2.1
-  unset RUBY_HEAP_MIN_SLOTS
-  unset RUBY_HEAP_SLOTS_INCREMENT
-  unset RUBY_HEAP_SLOTS_GROWTH_FACTOR
-  unset RUBY_GC_MALLOC_LIMIT
-  unset RUBY_HEAP_FREE_MIN
-}
-
-function ruby_env_setup_gc() {
-  # Slightly shonky version detection here, but sod it, how likely is ruby to
-  # hit 2.10.0 without rolling over a major version?
-  case $RUBY_VERSION in
-    2.[1-9].[0-9])
-      export RUBY_GC_HEAP_INIT_SLOTS=1200000
-      export RUBY_GC_HEAP_FREE_SLOTS=100000
-      export RUBY_GC_HEAP_GROWTH_FACTOR=1.25
-      export RUBY_GC_HEAP_GROWTH_MAX_SLOTS=300000
-      ;;
-    *)
-      export RUBY_HEAP_MIN_SLOTS=1000000
-      export RUBY_HEAP_SLOTS_INCREMENT=100000
-      export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-      export RUBY_GC_MALLOC_LIMIT=100000000
-      export RUBY_HEAP_FREE_MIN=500000
-      ;;
-  esac
-}
-
 function ruby_env_auto() {
   local current_dir="$PWD"
   local version
@@ -51,9 +16,7 @@ function ruby_env_auto() {
       else
         [[ -n "$RUBY_GEMSET" ]] && gem_home -
         RUBY_AUTO_VERSION="$version"
-        ruby_env_reset_gc
         chruby "$version"
-        ruby_env_setup_gc
         if [[ -f "$current_dir/.gemset" ]]; then
           gem_dir=${current_dir//\//\%}
           gem_dir=${gem_dir// /_}
