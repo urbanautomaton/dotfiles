3a70bfc9228c0958dc32de0c12a6319dc495a3ac
Simon Coffey <simon@urbanautomaton.com>
2025-05-08 10:01:23 +0530

    Remove unhelpful aliases
    The workspace ones aren't relevant, and `stty sane` does the job of
    fix_terminal, which for some reason I could never remember the name of
    anyway.

--- bashrc.d/03_bash_aliases.sh
+++ bashrc.d/03_bash_aliases.sh
@@ -23,9 +23,6 @@ alias ga='git add'
 alias gd='git diff'
 
 alias ws='workspace'
-alias blog='workspace -n ua ~/dev/sites/urbanautomaton.com'
-alias fl='workspace -d "mysql -u root -D futurelearn_development --host 127.0.0.1" ~/dev/futurelearn/futurelearn'
-alias fix_terminal='stty echo icanon icrnl dsusp ^Y lnext ^V'
 
 alias ci="hub ci-status -v \`git rev-parse --abbrev-ref HEAD\` | grep jenkins.futurelearn.dev | cut -f3 | xargs open -a /Applications/Google\ Chrome.app"
 
