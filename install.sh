d9793d72fbf690102cba13a769926d0dc2b26510
Simon Coffey <simon@urbanautomaton.com>
2026-01-11 16:56:33 +0000

    Fix install script

--- install.sh
+++ install.sh
@@ -35,42 +35,53 @@ readonly DOTFILES="
   vimrc
 "
 
+echo "doing directories"
 for dir in $DOTDIRECTORIES; do
-  if [[ ! -d "$dir" ]]; then
-    echo "Symlinking: ${HOME}/dotfiles/${dir} => ${HOME}/.${dir}"
-    $dircmd "${HOME}/dotfiles/${dir}" "${HOME}/.${dir}"
+  dest="${HOME}/.${dir}"
+
+  if [[ ! -d "$dest" ]]; then
+    echo "Symlinking: ${HOME}/dotfiles/${dir} => ${dest}"
+    $dircmd "${HOME}/dotfiles/${dir}" "${dest}"
   else
     echo "    Exists: ${HOME}/.${dir}"
   fi
 done
 
+echo "doing files"
 for file in $DOTFILES; do
-  if [[ ! -f "$file" ]]; then
-    echo "Symlinking: ${HOME}/dotfiles/${file} => ${HOME}/.${file}"
-    ln -sf "${HOME}/dotfiles/${file}" "${HOME}/.${file}"
+  dest="${HOME}/.${file}"
+
+  if [[ ! -f "$dest" ]]; then
+    echo "Symlinking: ${HOME}/dotfiles/${file} => ${dest}"
+    ln -sf "${HOME}/dotfiles/${file}" "${dest}"
   else
 
     echo "    Exists: ${HOME}/.${file}"
   fi
 done
 
+echo "doing bashrc.d"
 mkdir -p ~/.bashrc.d
 for script_file in ~/dotfiles/bashrc.d/*; do
   script_name=$(basename "${script_file}")
-  if [[ ! -f "${script_file}" ]]; then
-    echo "Symlinking: ${script_file} => ${HOME}/.bashrc.d/${script_name}"
-    ln -sf "${script_file}" "${HOME}/.bashrc.d/${script_name}"
+  dest="${HOME}/.bashrc.d/${script_name}"
+
+  if [[ ! -f "${dest}" ]]; then
+    echo "Symlinking: ${script_file} => ${dest}"
+    ln -sf "${script_file}" "${dest}"
   else
     echo "    Exists: ${HOME}/.bashrc.d/${script_name}"
   fi
 done
 
+echo "doing bin"
 mkdir -p ~/bin
 for script_file in ~/dotfiles/bin/*; do
   script_name=$(basename "${script_file}")
-  if [[ ! -f "${script_file}" ]]; then
+  destination="${HOME}/bin/${script_name}"
+  if [[ ! -f "${destination}" ]]; then
     echo "Symlinking: ${script_file} => ${HOME}/bin/${script_name}"
-    ln -sf "${script_file}" "${HOME}/bin/${script_name}"
+    ln -sf "${script_file}" "${destination}"
   else
     echo "    Exists: ${HOME}/bin/${script_name}"
   fi
